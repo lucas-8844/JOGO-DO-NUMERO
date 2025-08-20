042d9a704c4c44c26fb4e6e648f3450ba563fb11
lucas-8844 <carvalho.peres88@gmail.com>
2025-08-20 18:08:17 -0300

    mensagem de erro

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import random
-numero = int(input("escreva um numero de 1 a 10:"))
-numero_certo = random.randint(1,10)
-tentativa = 0
-while numero != numero_certo:
+try:
+  numero = int(input("escreva um numero de 1 a 10:"))
+  numero_certo = random.randint(1,10)
+  tentativa = 0
+  while numero != numero_certo:
         tentativa += 1
         if numero > numero_certo:
          print("Muito Alto, tente um numero mais baixo:")
         else:
              print("muito baixo, tente um numero maior:")
         numero = int(input("escreva um numero de 1 a 10:"))
-print(f"Parabéns, você acertou em {tentativa} tentativas!") 
+  print(f"Parabéns, você acertou em {tentativa} tentativas!") 
+except:
+ print("Entrada não é valida")
